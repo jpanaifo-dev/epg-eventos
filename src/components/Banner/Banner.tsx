94115097bc2e6e42426003aabeb0d66f59c6f693
José Jefferson Santos Panaifo <jsantosp@postgradounap.edu.pe>
2024-06-18 10:36:07 -0500

    chore: Update landing page with anniversary and scientific event details

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -13,8 +13,8 @@ export default function Banner(props: IProps) {
         alt="Banner"
         className="w-full h-full object-cover"
       />
-      <section className="container absolute z-10 top-0 right-0 bottom-0 left-0 bg-black/40 w-full h-full">
-        <div className="w-full h-full text-white flex flex-col justify-center items-center">
+      <section className="absolute z-10 top-0 right-0 bottom-0 left-0 bg-black/40 w-full h-full">
+        <div className="container w-full h-full text-white flex flex-col justify-center items-center">
           <div>
             <h1 className="text-white text-2xl">{title}</h1>
             <p className="text-6xl">{description}</p>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,7 +9,11 @@ module.exports = {
     './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+      },
+    },
   },
   darkMode: 'class',
   plugins: [nextui()],
