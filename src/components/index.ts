f7d8f90479397550657ff0429089d337860c14d3
José Jefferson Santos Panaifo <jsantosp@postgradounap.edu.pe>
2024-06-18 10:17:24 -0500

    feat: Add Banner component to components index

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,6 @@ import Header from './Header/Header'
 import Footer from './Footer/Footer.astro'
 import BaseHead from './BaseHead.astro'
 import Landing from './Landing/Landing'
+import Banner from './Banner/Banner'
 
-export { Header, Footer, BaseHead, Landing }
+export { Header, Footer, BaseHead, Landing, Banner }
