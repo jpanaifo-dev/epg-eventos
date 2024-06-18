57d30bbd2bf651d3662ae558463bec2e3d2b11c9
José Jefferson Santos Panaifo <jsantosp@postgradounap.edu.pe>
2024-06-18 10:10:31 -0500

    chore: Update landing page with anniversary and scientific event details

--- src/components/Landing/Landing.tsx
+++ src/components/Landing/Landing.tsx
@@ -8,11 +8,16 @@ export default function Landing() {
           className="w-full h-screen rounded-none"
         />
         <main className="absolute z-10 top-0 right-0 bottom-0 left-0 bg-black/40 w-full h-full">
-          <section className="w-full h-full">
+          <section className="w-full h-full text-white">
             <h1 className="text-white">
               37
               <span className="text-2xl">avo</span>
             </h1>
+            <p>Aniversario &</p>
+            <h1 className="text-white">
+              I <span className="text-2xl">er</span>
+            </h1>
+            <p>Encuentro Científico</p>
           </section>
         </main>
       </div>
