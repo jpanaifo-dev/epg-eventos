2e8f42b2d692921f3741c751ebadbf29cd31880c
José Jefferson Santos Panaifo <jsantosp@postgradounap.edu.pe>
2024-06-18 10:33:50 -0500

    chore: Update landing page with anniversary and scientific event details

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -7,10 +7,11 @@ interface IProps {
 export default function Banner(props: IProps) {
   const { title, description, image } = props
   return (
-    <div className="relative w-full h-full">
+    <div className="relative w-full h-[500px] max-h-[500px]">
       <img
         src={`/images/${image}`}
         alt="Banner"
+        className="w-full h-full object-cover"
       />
       <section className="container absolute z-10 top-0 right-0 bottom-0 left-0 bg-black/40 w-full h-full">
         <div className="w-full h-full text-white flex flex-col justify-center items-center">
