027a2cc9a674303837ce29bf5308da39d69bf63d
José Jefferson Santos Panaifo <jsantosp@postgradounap.edu.pe>
2024-06-18 10:19:42 -0500

    chore: Update landing page with anniversary and scientific event details

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,5 +1,25 @@
-interface IProps {}
+interface IProps {
+  title: string
+  description: string
+  image: string
+}
 
-export default function Banner() {
-  return <></>
+export default function Banner(props: IProps) {
+  const { title, description, image } = props
+  return (
+    <div className="relative w-full h-full">
+      <img
+        src={`/images/${image}`}
+        alt="Banner"
+      />
+      <section className="container absolute z-10 top-0 right-0 bottom-0 left-0 bg-black/40 w-full h-full">
+        <div className="w-full h-full text-white flex flex-col justify-center items-center">
+          <div>
+            <h1 className="text-white text-2xl">{title}</h1>
+            <p className="text-6xl">{description}</p>
+          </div>
+        </div>
+      </section>
+    </div>
+  )
 }
